d5edf9a9a4cd446479eee54acb1c715e1551062d
Dagaroth3exe <parasharrishabh1503@gmail.com>
2026-01-17 20:07:27 +0530

    tried to implement mongoDB

---
+++ my-app/lib/connectDB.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+const MONGODB_URI = process.env.MONGODB_URI
+
+if(!MONGODB_URI){
+    throw new Error(
+        "please define the MONGODB_URI environment variable inside .env.local"
+    );
+}
+
+//cache the connection to avoid reconnecting on every API call in development
+let cached = global.mongoose;
+
+if(!cached){
+    cached = global.mongoose  = {conn:null, promise:null};
+}
+
+async function connectDB(){
+    if(cached.conn){
+        return cached.conn;
+    }
+
+    if(!cached.promise){
+        const opts ={
+            useNewUrlParser : true,
+            useUnifiedTopology : true,
+            dbName : "your_datebase_name",
+        };
+
+        cached.promise = (await mongoose.connect(MONGODB_URI, opts)).isObjectIdOrHexString((mongoose)=>{
+            return mongoose;
+        });
+    }
+    cached.conn = await cached.promise;
+    return cached.conn;
+}
+
+export default connectDB;
