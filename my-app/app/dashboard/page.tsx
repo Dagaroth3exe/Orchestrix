138471cda292c47399bf177ae9e9a2057692324f
Dagaroth3exe <parasharrishabh1503@gmail.com>
2026-02-08 15:15:22 +0530

    UI: removed the sidebar component

--- my-app/app/dashboard/page.tsx
+++ my-app/app/dashboard/page.tsx
@@ -1,10 +1,5 @@
-import SideNav from "../components/SideNav";
-
 export default function DashboardPage() {
   return (
-    <div className="min-h-screen w-full text-white">
-      <SideNav/>
-      
-    </div>
+    <div className="min-h-screen w-full text-white" />
   );
 }
